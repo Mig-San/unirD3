f9b440d7f3ccab6c579aea53e376313ccc6ad227
Mig-San <77906078+Mig-San@users.noreply.github.com>
2021-01-24 15:29:13 -0600

    grafico
    grafico final

--- app2.js
+++ app2.js
@@ -31,12 +31,11 @@ x = d3.scaleBand()
       // d. Parámetros extras del escalador
       .paddingInner(0.1)
       .paddingOuter(0.3)
-
-// f. Para convertir datos ordinales usamos scaleOrdinal
+      
 color = d3.scaleOrdinal()
-          // .range(['red', 'green', 'blue', 'yellow'])
+           .range(['#660000', '#990000','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CC0000'])
           // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
-          .range(d3.schemeDark2)
+          //.range(d3.schemeDark2)
 
 // i. Ejes lo primero es crear un "grupo"
 xAxisGroup = g.append('g')
@@ -50,7 +49,7 @@ titulo = g.append('text')
           .attr('x', `${ancho / 2}px`)
           .attr('y', '-5px')
           .attr('text-anchor', 'middle')
-          .text('La profundidad de los 3 sismos mas destructivos en México')
+          .text('La profundidad de los sismos de mayor magnitud en México')
           .attr('class', 'titulo-grafica')
 
 dataArray = []
@@ -68,20 +67,20 @@ function render(data) {
         .style('height', '0px')
         .style('y', `${y(0)}px`)
         .style('fill', '#000')
-        .style('x', d => x(d.edificio) + 'px')
+        .style('x', d => x(d.fechamag) + 'px')
       .transition()
       // https://bl.ocks.org/d3noob/1ea51d03775b9650e8dfd03474e202fe
       .ease(d3.easeElastic)
       .duration(1500)
-        .style('y', d => (y(d.oficial)) + 'px')
-        .style('height', d => (alto - y(d.oficial)) + 'px')
+        .style('y', d => (y(d.profundidad)) + 'px')
+        .style('height', d => (alto - y(d.profundidad)) + 'px')
         .style('fill', d => color(d.region))
         .style('width', d => `${x.bandwidth()}px`)
 
       // j. call que sirve para crear los ejes
       yAxisCall = d3.axisLeft(y)
                     .ticks(3)
-                    .tickFormat(d => `${d} m.`)
+                    .tickFormat(d => `${d} Km.`)
       yAxisGroup.call(yAxisCall)
 
       xAxisCall = d3.axisBottom(x)
@@ -97,36 +96,27 @@ function render(data) {
 d3.csv('sismos3.csv')
 .then(function(data) {
   data.forEach(d => {
-    d.oficial = +d.oficial
+    d.profundidad = +d.profundidad
     d.ano = +d.ano
-    d.antena = +d.antena
-    d.piso = +d.piso
-    d.ultimopiso = +d.ultimopiso
     d.puesto = +d.puesto
   })
 
-  // e. Seleccionar solo los diez edificios más altos
-  data = data.slice(0, 15)
+  // e. Seleccionar los sismos mas altos
+  data = data.slice(0, 18)
 
   this.dataArray = data
 
-  // Calcular la altura más alta dentro de
-  // los datos (columna "oficial")
-  maxy = d3.max(data, d => d.oficial)
+  // Calcular la profundidad más alta dentro de
+  // los datos (columna "profundidad")
+  maxy = d3.max(data, d => d.profundidad)
 
   // Creamos una función para calcular la altura
   // de las barras y que quepan en nuestro canvas
   y.domain([0, maxy])
 
-  // b. Poner el dominio del escalador x, convertir los nombres de los
-  //    edificios en valores de X para ubicar las barras
-  x.domain(data.map(d => d.edificio))
-  // console.log('Edificios')
-  // console.log(data.map(d => d.edificio))
-
-  // g. Al igual que con los edificios, voy a poner las regiones
-  //    como dominio del escalador 'color'
-  color.domain(data.map(d => d.region))
+  // b. Poner el dominio del escalador x, convertir las fechas de los
+  //    sismos en valores de X para ubicar las barras
+  x.domain(data.map(d => d.fechamag))
 
   // V. Despliegue
   render(dataArray)
